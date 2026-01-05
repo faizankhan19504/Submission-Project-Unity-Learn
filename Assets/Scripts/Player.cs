030e122c1fa080c88a6ca458f91bfe1b394acd48
Faizan khan <muhammadfaizan19504@gmail.com>
2026-01-05 13:00:29 +0500

    Second Commit
    Player Movement and BoundaryCheck Logic.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float Speed = 10.0f;
+    private float xRange = 9.0f;
+    private void Update()
+    {
+        PlayerMovement();
+        BoundaryCheck();
+    }
+    private void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+        }
+    }
+    private void BoundaryCheck()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
